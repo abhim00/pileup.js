e04cfd4035cb00f6ba2f61c9cd0aa84f5a150796
Abhishek Mahesh <abhim00@berkeley.edu>
2019-02-11 09:34:18 -0800

    added express module, local_files_folder, and LocalFile-test and local server

---
+++ abhiREADME.md
@@ -0,0 +1,8 @@
+-Can use express.js to serve static, local files
+- can use “ps aux | grep node” to look at all current node processes
+- kill them using “kill -9 PID”
+- if running n background kill node server using CTRL-C
+
+there was a problem with phantomjs not being installed on my computer
+--> it was resolved by using brew cask install phantomjs
+
---
+++ src/test/LocalFile-test.js
@@ -0,0 +1,143 @@
+/* @flow */
+'use strict';
+
+import {expect} from 'chai';
+
+import jBinary from 'jbinary';
+
+import LocalFile from '../main/LocalFile';
+
+describe('LocalFile', () => {
+  function bufferToText(buf) {
+    return new jBinary(buf).read('string');
+  }
+
+  it('should fetch a subset of a file', function(): any {
+    var f = new LocalFile('0to9.txt');
+    var promisedData = f.getBytes(4, 5);
+
+    expect(f.numNetworkRequests).to.equal(1);
+    return promisedData.then(buf => {
+      expect(buf.byteLength).to.equal(5);
+      expect(bufferToText(buf)).to.equal('45678');
+    });
+  });
+
+  it('should fetch subsets from cache', function(): any {
+    var f = new LocalFile('0to9.txt');
+    return f.getBytes(0, 10).then(buf => {
+      expect(buf.byteLength).to.equal(10);
+      expect(bufferToText(buf)).to.equal('0123456789');
+      expect(f.numNetworkRequests).to.equal(1);
+      return f.getBytes(4, 5).then(buf => {
+        expect(buf.byteLength).to.equal(5);
+        expect(bufferToText(buf)).to.equal('45678');
+        expect(f.numNetworkRequests).to.equal(1);  // it was cached
+      });
+    });
+  });
+
+  it('should fetch entire files', function(): any {
+    var f = new LocalFile('0to9.txt');
+    return f.getAll().then(buf => {
+      expect(buf.byteLength).to.equal(11);
+      expect(bufferToText(buf)).to.equal('0123456789\n');
+      expect(f.numNetworkRequests).to.equal(1);
+    });
+  });
+
+  it('should determine file lengths', function(): any {
+    var f = new LocalFile('0to9.txt');
+    return f.getSize().then(size => {
+      expect(size).to.equal(11);
+      // TODO: make sure this was a HEAD request
+      expect(f.numNetworkRequests).to.equal(1);
+    });
+  });
+
+  it('should get file lengths from full requests', function(): any {
+    var f = new LocalFile('0to9.txt');
+    return f.getAll().then(buf => {
+      expect(f.numNetworkRequests).to.equal(1);
+      return f.getSize().then(size => {
+        expect(size).to.equal(11);
+        expect(f.numNetworkRequests).to.equal(1);  // no additional requests
+      });
+    });
+  });
+
+  it('should get file lengths from range requests', function(): any {
+    var f = new LocalFile('0to9.txt');
+    return f.getBytes(4, 5).then(buf => {
+      expect(f.numNetworkRequests).to.equal(1);
+      return f.getSize().then(size => {
+        expect(size).to.equal(11);
+        expect(f.numNetworkRequests).to.equal(1);  // no additional requests
+      });
+    });
+  });
+
+  it('should cache requests for full files', function(): any {
+    var f = new LocalFile('0to9.txt');
+    return f.getAll().then(buf => {
+      expect(buf.byteLength).to.equal(11);
+      expect(bufferToText(buf)).to.equal('0123456789\n');
+      expect(f.numNetworkRequests).to.equal(1);
+      return f.getAll().then(buf => {
+        expect(buf.byteLength).to.equal(11);
+        expect(bufferToText(buf)).to.equal('0123456789\n');
+        expect(f.numNetworkRequests).to.equal(1);  // still 1
+      });
+    });
+  });
+
+  it('should serve range requests from cache after getAll', function(): any {
+    var f = new LocalFile('0to9.txt');
+    return f.getAll().then(buf => {
+      expect(buf.byteLength).to.equal(11);
+      expect(bufferToText(buf)).to.equal('0123456789\n');
+      expect(f.numNetworkRequests).to.equal(1);
+      return f.getBytes(4, 5).then(buf => {
+        expect(buf.byteLength).to.equal(5);
+        expect(bufferToText(buf)).to.equal('45678');
+        expect(f.numNetworkRequests).to.equal(1);  // still 1
+      });
+    });
+  });
+
+  it('should reject requests to a non-existent file', function(): any {
+    var f = new LocalFile('nonexistent-file.txt');
+    return f.getAll().then(buf => {
+      throw 'Requests for non-existent files should not succeed';
+    }, err => {
+      // The majority of the browsers will return 404
+      // and a minority (like PhantomJS) will fail fast
+      // (more information: https://github.com/ariya/phantomjs/issues/11195)
+      expect(err).to.match(/404|^Request.*failed/);
+      expect(err).to.match(/nonexistent/);
+    });
+  });
+
+  it('should truncate requests past EOF', function(): any {
+    var f = new LocalFile('0to9.txt');
+    var promisedData = f.getBytes(4, 100);
+
+    return promisedData.then(buf => {
+      expect(buf.byteLength).to.equal(7);
+      expect(bufferToText(buf)).to.equal('456789\n');
+      expect(f.numNetworkRequests).to.equal(1);
+      return f.getBytes(6, 90).then(buf => {
+        expect(buf.byteLength).to.equal(5);
+        expect(bufferToText(buf)).to.equal('6789\n');
+        expect(f.numNetworkRequests).to.equal(1);
+      });
+    });
+  });
+
+  it('should fetch entire files as strings', function(): any {
+    var f = new LocalFile('0to9.txt');
+    return f.getAllString().then(txt => {
+      expect(txt).to.equal('0123456789\n');
+    });
+  });
+});
